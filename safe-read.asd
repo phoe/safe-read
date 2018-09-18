b01f723c3b0036228360529833028fe49534a55b
Michał "phoe" Herda <phoe@disroot.org>
2018-09-18 20:10:48 +0200

    add a test system

--- safe-read.asd
+++ safe-read.asd
@@ -6,10 +6,18 @@
 (asdf:defsystem #:safe-read
   :description "A variant of READ secure against internbombing, excessive input
 and macro characters."
-  :author "Michał \"phoe\" Herda <phoe@openmailbox.org>"
+  :author "Michał \"phoe\" Herda <phoe@disroot.org>"
   :license "BSD 2-clause"
   :depends-on (#:local-time
                #:trivial-garbage)
   :serial t
   :components ((:file "package")
                (:file "safe-read")))
+
+(asdf:defsystem #:safe-read/test
+  :description "Test suite for SAFE-READ"
+  :author "Michał \"phoe\" Herda <phoe@disroot.org>"
+  :license "BSD 2-clause"
+  :depends-on (:safe-read)
+  :components ((:file "test"))
+  :perform (test-op (o c) (symbol-call :safe-read :test)))
--- test.lisp
+++ test.lisp
@@ -8,81 +8,91 @@
 (in-package #:safe-read)
 
 ;;;; CONDITION-KEY test
-(let ((condition (make-condition 'error)))
-  (assert (string= 'error (condition-key condition))))
+(defun test-1 ()
+  (let ((condition (make-condition 'error)))
+    (assert (string= 'error (condition-key condition)))))
 
 ;;;; WITH-TEMP-PACKAGE test
-(let ((*package* (find-package "COMMON-LISP")))
-  (with-temp-package
-    (assert (not (eq *package* (find-package "COMMON-LISP"))))
-    (assert (search "TEMP-PKG-" (package-name *package*)))))
+(defun test-2 ()
+  (let ((*package* (find-package "COMMON-LISP")))
+    (with-temp-package
+      (assert (not (eq *package* (find-package "COMMON-LISP"))))
+      (assert (search "TEMP-PKG-" (package-name *package*))))))
 
 ;;;; SAFE-READTABLE test
-(let ((*readtable* %safe-readtable%))
-  (flet ((errors (string) (signals malformed-input (read-from-string string)))
-         (oerrors (string) (signals (and error (not malformed-input))
-                             (read-from-string string)))
-         (generate (char) (coerce (list #\# char) 'string)))
-    (let* ((sharpsign-chars '(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L
-                              #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X
-                              #\Y #\Z #\# #\' #\( #\) #\* #\= #\\ #\| #\+ #\-
-                              #\.))
-           (sharpsign-strings (mapcar #'generate sharpsign-chars)))
-      (mapcar #'oerrors '("\"" "(" ")" "#"))
-      (mapcar #'errors (list* "'" ";" "`" "," sharpsign-strings))
-      (assert (eq 'test (read-from-string "#:test"))))))
+(defun test-3 ()
+  (let ((*readtable* %safe-readtable%))
+    (flet ((errors (string) (signals malformed-input (read-from-string string)))
+           (oerrors (string) (signals (and error (not malformed-input))
+                               (read-from-string string)))
+           (generate (char) (coerce (list #\# char) 'string)))
+      (let* ((sharpsign-chars '(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L
+                                #\M #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X
+                                #\Y #\Z #\# #\' #\( #\) #\* #\= #\\ #\| #\+ #\-
+                                #\.))
+             (sharpsign-strings (mapcar #'generate sharpsign-chars)))
+        (mapcar #'oerrors '("\"" "(" ")" "#"))
+        (mapcar #'errors (list* "'" ";" "`" "," sharpsign-strings))
+        (let ((result (read-from-string "#:test")))
+          (assert (symbolp result))
+          (assert (string= "TEST" result)))))))
 
 ;;;; SAFE-READ-NO-BUFFER and SAFE-READ-BUFFER
-(let* ((*max-input-size* 20)
-       (data-0 '(#:test))
-       (data-1 '(123456789012345678))
-       (data-2 '(1234567890123456789))
-       (data-3 '(#(1 2 3 4 5)))
-       (data-4 (list (uiop:strcat "a" (string #\Newline) "b"))))
-  (labels ((streamify (data) (make-string-input-stream (format nil "~S" data)))
-           (nread (data) (safe-read-no-buffer (streamify data)))
-           (bread (data) (safe-read-buffer (streamify data)))
-           (symbol-test (fn data) (string= (first data) (first (funcall fn data)))))
-    ;; SAFE-READ-NO-BUFFER
-    (let ((stream (streamify data-0)))
-      (assert (string= (first data-0) (first (safe-read-no-buffer stream))))
-      (assert (signals end-of-file (safe-read-no-buffer stream))))
-    (assert (equal data-1 (nread data-1)))
-    (assert (signals input-size-exceeded (nread data-2)))
-    (assert (signals malformed-input (nread data-3)))
-    (let ((stream (streamify data-4)))
-      (assert (null (safe-read-no-buffer stream)))
-      (signals malformed-input (read-limited-line stream)))
-    ;; SAFE-READ-BUFFER
-    (assert (symbol-test #'bread data-0))
-    (assert (equal data-1 (bread data-1)))
-    (assert (signals input-size-exceeded (bread data-2)))
-    (assert (signals malformed-input (bread data-3)))
-    (let ((stream (streamify data-4)))
-      (assert (null (safe-read-buffer stream)))
-      (assert (equal data-4 (safe-read-buffer stream))))))
+(defun test-4 ()
+  (let* ((*max-input-size* 20)
+         (data-0 '(#:test))
+         (data-1 '(123456789012345678))
+         (data-2 '(1234567890123456789))
+         (data-3 '(#(1 2 3 4 5)))
+         (data-4 (list (uiop:strcat "a" (string #\Newline) "b"))))
+    (labels ((streamify (data) (make-string-input-stream (format nil "~S" data)))
+             (nread (data) (safe-read-no-buffer (streamify data)))
+             (bread (data) (safe-read-buffer (streamify data)))
+             (symbol-test (fn data) (string= (first data) (first (funcall fn data)))))
+      ;; SAFE-READ-NO-BUFFER
+      (let ((stream (streamify data-0)))
+        (assert (string= (first data-0) (first (safe-read-no-buffer stream))))
+        (assert (signals end-of-file (safe-read-no-buffer stream))))
+      (assert (equal data-1 (nread data-1)))
+      (assert (signals input-size-exceeded (nread data-2)))
+      (assert (signals malformed-input (nread data-3)))
+      (let ((stream (streamify data-4)))
+        (assert (null (safe-read-no-buffer stream)))
+        (signals malformed-input (read-limited-line stream)))
+      ;; SAFE-READ-BUFFER
+      (assert (symbol-test #'bread data-0))
+      (assert (equal data-1 (bread data-1)))
+      (assert (signals input-size-exceeded (bread data-2)))
+      (assert (signals malformed-input (bread data-3)))
+      (let ((stream (streamify data-4)))
+        (assert (null (safe-read-buffer stream)))
+        (assert (equal data-4 (safe-read-buffer stream)))))))
 
 ;; TODO add test with leading whitespace
 
 ;; SAFE-READ test
-(flet ((newline (string) (uiop:strcat string (string #\Newline))))
-  (let* ((strings '("(1 2 3 4)"
-                    "    (5 6 7 8)"
-                    "(9 8 7"
-                    " 6 5 4)"
-                    "(3 2 1"
-                    "0 1 2"
-                    "          "
-                    "3 4 5)"))
-         (string (apply #'uiop:strcat (mapcar #'newline strings)))
-         (stream (make-string-input-stream string)))
-    (assert (equal '(1 2 3 4) (safe-read stream)))
-    (assert (equal '(5 6 7 8) (safe-read stream)))
-    (assert (null (safe-read stream)))
-    (assert (equal '(9 8 7 6 5 4) (safe-read stream)))
-    (assert (null (safe-read stream)))
-    (assert (null (safe-read stream)))
-    (assert (null (safe-read stream)))
-    (assert (equal '(3 2 1 0 1 2 3 4 5) (safe-read stream)))))
+(defun test-5 ()
+  (flet ((newline (string) (uiop:strcat string (string #\Newline))))
+    (let* ((strings '("(1 2 3 4)"
+                      "    (5 6 7 8)"
+                      "(9 8 7"
+                      " 6 5 4)"
+                      "(3 2 1"
+                      "0 1 2"
+                      "          "
+                      "3 4 5)"))
+           (string (apply #'uiop:strcat (mapcar #'newline strings)))
+           (stream (make-string-input-stream string)))
+      (assert (equal '(1 2 3 4) (safe-read stream)))
+      (assert (equal '(5 6 7 8) (safe-read stream)))
+      (assert (null (safe-read stream)))
+      (assert (equal '(9 8 7 6 5 4) (safe-read stream)))
+      (assert (null (safe-read stream)))
+      (assert (null (safe-read stream)))
+      (assert (null (safe-read stream)))
+      (assert (equal '(3 2 1 0 1 2 3 4 5) (safe-read stream))))))
 
 ;; TODO one of the tests hangs indefinitely, check it
+
+(defun test ()
+  (test-1) (test-2) (test-3) (test-4) (test-5))
